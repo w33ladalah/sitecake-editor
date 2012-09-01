dcb0eeb9f46a7919a669b8d1e800d164018a117c
Nikola Ivancevic <ivancevic.nikola@gmail.com>
2012-09-02 01:49:17 +0200

    Moving CSSStyleDeclaration to util/dom package

--- sitecake-editor-commons/src/main/java/com/sitecake/commons/client/util/dom/CSSStyleDeclaration.java
+++ sitecake-editor-commons/src/main/java/com/sitecake/commons/client/util/dom/CSSStyleDeclaration.java
@@ -1,7 +1,36 @@
 package com.sitecake.commons.client.util.dom;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Element;
 
 public class CSSStyleDeclaration extends JavaScriptObject {
-	protected CSSStyleDeclaration() {}
+
+	protected CSSStyleDeclaration() {
+	}
+	
+	public static final native CSSStyleDeclaration get(Element element)/*-{
+		return $doc.defaultView.getComputedStyle(element, '');
+	}-*/;
+
+	public final native String getPropertyValue(String propertyName)/*-{
+		return this.getPropertyValue(propertyName);
+	}-*/;
+	
+	public final int getPropertyValueInt(String propertyName) {
+		String value = getPropertyValue(propertyName);
+		int result = 0;
+		if ( propertyName != null && !"".equals(propertyName) ) {
+			result = Integer.valueOf(value.replaceAll("px", "").trim());
+		}
+		return result;
+	}
+	
+	public final double getPropertyValueDouble(String propertyName) {
+		String value = getPropertyValue(propertyName);
+		double result = 0;
+		if ( propertyName != null && !"".equals(propertyName) ) {
+			result = Double.valueOf(value.replaceAll("px", "").trim());
+		}
+		return result;
+	}	
 }
